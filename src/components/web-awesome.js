9bf91b85f71150bf1cb670a10178d7b0af2cc813
Daniel Buchner <danieljb2@gmail.com>
2024-08-13 17:43:29 -0500

    first commit

---
+++ src/components/web-awesome.js
@@ -0,0 +1,3 @@
+
+import 'https://early.webawesome.com/webawesome@3.0.0-alpha.2/dist/components/avatar/avatar.js';
+import 'https://early.webawesome.com/webawesome@3.0.0-alpha.2/dist/components/tab-group/tab-group.js';
