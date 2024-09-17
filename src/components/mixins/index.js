1a5cee2f3cac9bb7a8c741d02897bf7e604788b0
Daniel Buchner <csuwildcat@users.noreply.github.com>
2024-09-17 20:25:39 +0000

    Updates

--- src/components/mixins/index.js
+++
@@ -1,9 +0,0 @@
-import { LitElement } from 'lit'
-
-LitElement.with = function(...mixins) {
-  return mixins.reduce((Base, mixin) => mixin(Base), this);
-};
-
-export { State } from './state.js';
-export { Query } from './query.js';
-export { Spinner, SpinnerStyles } from './spinner.js';
--- src/components/web-awesome.js
+++
@@ -1,7 +0,0 @@
-
-import 'https://early.webawesome.com/webawesome@3.0.0-alpha.2/dist/components/avatar/avatar.js';
-import 'https://early.webawesome.com/webawesome@3.0.0-alpha.2/dist/components/button/button.js';
-import 'https://early.webawesome.com/webawesome@3.0.0-alpha.2/dist/components/dialog/dialog.js';
-import 'https://early.webawesome.com/webawesome@3.0.0-alpha.2/dist/components/icon/icon.js';
-import 'https://early.webawesome.com/webawesome@3.0.0-alpha.2/dist/components/icon-button/icon-button.js';
-import 'https://early.webawesome.com/webawesome@3.0.0-alpha.2/dist/components/tab-group/tab-group.js';
